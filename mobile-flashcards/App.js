f4ffeb6d698784f2f549cd5961a864750f81c868
Gil Akos <gil@astra.finance>
2018-04-30 21:24:13 -0700

    Feat: TabBarNav

--- mobile-flashcards/App.js
+++ mobile-flashcards/App.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react'
 import { ConnectedRouter } from 'react-router-redux'
 import createHistory from 'history/createMemoryHistory'
-import { Navigation, Card } from 'react-router-navigation'
+import { TabNavigator, TabBarBottom } from 'react-navigation'
+import { View, Platform, StatusBar } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
 
 import DeckList from './modules/deck_list/DeckListView'
-import DeckIndividual from './modules/deck_individual/DeckIndividualView'
+import DeckNew from './modules/deck_new/DeckNewView'
+
+import * as colors from './utils/colors'
 
 const history = createHistory()
+
+function FlashCardsStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const FlashCardTabs = TabNavigator(
+  {
+    Decks: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-albums" size={30} color={tintColor} />
+        )
+      }
+    },
+    NewDeck: {
+      screen: DeckNew,
+      navigationOptions: {
+        tabBarLabel: 'Add Deck',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-add-circle" size={30} color={tintColor} />
+        )
+      }
+     }
+  },
+  {
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: colors.LIGHTPURPLE,
+      inactiveTintColor: colors.SUPERGRAY
+    },
+    showIcon: true,
+    showLabel: true,
+    animationEnabled: false,
+    swipeEnabled: false
+  }
+)
 
 export default class App extends Component {
   render() {
     return (
-      <DeckList />
+      <View style={{ flex: 1 }}>
+        <FlashCardsStatusBar
+          backgroundColor={colors.LIGHTPURPLE}
+          barStyle="light-content"
+        />
+        <FlashCardTabs />
+      </View>
     )
   }
 }
--- mobile-flashcards/modules/deck_new/DeckNewView.js
+++ mobile-flashcards/modules/deck_new/DeckNewView.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Navigation, Card } from 'react-router-navigation'
 
-export default class DeckListView extends Component {
+export default class DeckNewView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Deck List View</Text>
+        <Text>Deck New View</Text>
       </View>
     )
   }
