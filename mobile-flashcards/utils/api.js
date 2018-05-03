9c8c5998643d95722470d58e878818bdc76b764f
Gil Akos <gil@astra.finance>
2018-05-03 11:19:27 -0700

    Feat: Flashcards API

---
+++ mobile-flashcards/utils/api.js
@@ -0,0 +1,44 @@
+import { AsyncStorage } from 'react-native'
+
+export const DECKS_KEY = 'flashcards:decks'
+
+function fetchDecks() {
+  return AsyncStorage.getItem(DECKS_KEY).then(results => {
+    return JSON.parse(results)
+  })
+}
+
+function fetchDeck(id) {
+  return AsyncStorage.getItem(DECKS_KEY).then(results => {
+    return results[id]
+  })
+}
+
+export function addDeck(title) {
+  return AsyncStorage.mergeItem(
+    DECKS_KEY,
+    JSON.stringify({
+      [title]: {
+        title,
+        questions: []
+      }
+    })
+  )
+}
+
+export function addCardToDeck(title, card) {
+  return AsyncStorage.getItem(DECKS_KEY).then(results => {
+    const decks = JSON.parse(results)
+    decks[title].questions.push(card)
+    AsyncStorage.setItem(DECKS_KEY, JSON.stringify(decks))
+  })
+}
+
+export function removeDeck(id) {
+  return AsyncStorage.getItem(DECKS_KEY).then(results => {
+    const decks = JSON.parse(results)
+    decks[id] = undefined
+    delete decks[id]
+    AsyncStorage.setItem(DECKS_KEY, JSON.stringify(decks))
+  })
+}
