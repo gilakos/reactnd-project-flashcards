5f9d5d3729f9787eb678cf739997c745fe211b0a
Gil Akos <gil@astra.finance>
2018-05-04 13:40:37 -0700

    Feat: Quiz show answer / question

--- mobile-flashcards/modules/DeckQuizView.js
+++ mobile-flashcards/modules/DeckQuizView.js
@@ -12,16 +12,46 @@ import _ from 'lodash'
 import * as colors from '../utils/colors'
 
 class DeckQuizView extends Component {
+  state = {
+    showAnswer: false,
+    question_index: 0,
+    answers_correct: 0
+  }
   render() {
     const { deck } = this.props
+    const question_progress = `${this.state.question_index + 1} / ${
+      deck.questions.length
+    }`
+    console.log(deck.questions)
     return (
       <View style={styles.container}>
-        <View style={styles.deckView}>
-          <Text style={styles.deckTitle}>{`${deck.title} Quiz`}</Text>
-          <Text style={styles.cardsInfo}>
-            Show Answer
-          </Text>
+        <View style={styles.progressView}>
+          <Text style={styles.cardsInfo}>{question_progress}</Text>
         </View>
+        {this.state.showAnswer === false ? (
+          <View style={styles.deckView}>
+            <Text style={styles.deckTitle}>
+              {deck.questions[this.state.question_index].question}
+            </Text>
+            <TouchableOpacity
+              onPress={() => this.setState({ showAnswer: true })}
+            >
+              <Text style={styles.cardsInfo}>Show Answer</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.deckView}>
+            <Text style={styles.deckTitle}>
+              {deck.questions[this.state.question_index].answer}
+            </Text>
+            <TouchableOpacity
+              onPress={() => this.setState({ showAnswer: false })}
+            >
+              <Text style={styles.cardsInfo}>Show Question</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
         <View style={styles.buttonView}>
           <TouchableOpacity
             style={[styles.actionButton, styles.buttonPrimary]}
@@ -47,9 +77,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.WHITE
   },
+  progressView: {
+    paddingVertical: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   deckView: {
     backgroundColor: colors.WHITE,
-    paddingVertical: 160,
+    paddingVertical: 120,
     paddingHorizontal: 20,
     alignItems: 'center',
     justifyContent: 'center',
@@ -58,7 +93,8 @@ const styles = StyleSheet.create({
   },
   deckTitle: {
     fontSize: 20,
-    color: colors.LIGHTPURPLE
+    color: colors.LIGHTPURPLE,
+    marginBottom: 20
   },
   cardsInfo: {
     fontSize: 14,
